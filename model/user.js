25e8739ecdffcca3ca3fd2e42a8856b3bdf0dac5
Qasim Laghari <go.jets.laghari@gmail.com>
2016-12-15 17:18:50 -0600

    Add bcrypt encryption

--- model/user.js
+++ model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 
 //Define the Model
@@ -21,6 +22,32 @@ const userSchema = new Schema({
   username: String,
   policies:[policySchema]
 });
+
+//On save hook, encrypt password
+userSchema.pre('save', function(next){
+
+  //Get access to the user model
+  const user = this;
+
+  //Only hash the password if it has been modified
+  if ( !user.isModified('password')) {
+    return next();
+  }
+
+  //Generate a salt
+  bcrypt.genSalt(10, function(err, salt){
+    if(err) { return next(err) }
+    //Hash the password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash){
+      if(err) { return next(err) }
+      //Over ride with encrypted password
+      user.password = hash;
+      next();
+    })
+
+  })
+
+})
 
 
 //Create the model class
