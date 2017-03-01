80f0cbce3510df068f8507430524aecbd6c82389
Qasim Laghari <go.jets.laghari@gmail.com>
2017-03-01 10:41:00 -0600

    Refactor send email

--- controllers/email.js
+++ controllers/email.js
@@ -1,9 +1,10 @@
+const api_key = 'key-aeef4a8a2f3aebef7a61dde586fae5e6';
+const domain = 'sandbox5077d4bfd5344f9b9efea5e3352d6837.mailgun.org';
+const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
 
 exports.sendEmail = function(req, res, next) {
 
-var api_key = 'key-aeef4a8a2f3aebef7a61dde586fae5e6';
-var domain = 'sandbox5077d4bfd5344f9b9efea5e3352d6837.mailgun.org';
-var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain})
 
 var data = {
   from: 'go.jets.laghari@gmail.com',
